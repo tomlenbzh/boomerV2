57d7eebd312d09ddb1f9ca94ac4964e7df3f91bf
tomlenbzh <thomas.lenoel@epitech.eu>
2018-12-20 15:13:28 +0100

    Login page - first draft

--- Boomer-RN-App/src/containers/home.js
+++ Boomer-RN-App/src/containers/home.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
-import { Platform, StatusBar, StyleSheet } from "react-native";
-import { View } from "react-native";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
 import {
   Container,
   Header,
@@ -15,20 +14,15 @@ import {
   Body,
   Icon,
   Text
-} from "native-base";
+} from 'native-base';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    ...Platform.select({
-      android: {
-        marginTop: StatusBar.currentHeight
-      }
-    })
-  }
-});
+// import { connect } from 'react-redux';
 
 export class Home extends Component {
+  // static propTypes = {
+  //   prop: PropTypes
+  // };
+
   constructor(props) {
     super(props);
     this.state = { loading: true };
@@ -36,14 +30,16 @@ export class Home extends Component {
 
   async componentWillMount() {
     await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf')
     });
     this.setState({ loading: false });
   }
 
-  static navigationOptions = {};
+  static navigationOptions = {
+    title: 'Boomer'
+  };
 
   render() {
     const { navigate } = this.props.navigation;
@@ -51,34 +47,36 @@ export class Home extends Component {
       return <Expo.AppLoading />;
     }
     return (
-      <View style={styles.container}>
-        <Container>
-          <Header>
-            <Left>
-              <Button transparent onPress={() => navigate("Home")}>
-                <Icon name="home" />
-              </Button>
-            </Left>
-            <Body>
-              <Title>Boomer</Title>
-            </Body>
-            <Button transparent onPress={() => navigate("Profile")}>
-              <Icon name="menu" />
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent onPress={() => navigate('Home')}>
+              <Icon name="home" />
             </Button>
-          </Header>
-          <Content>
-            <Text>Welcome on the home page</Text>
-          </Content>
-          <Footer>
-            <FooterTab>
-              <Button full>
-                <Text>Footer</Text>
-              </Button>
-            </FooterTab>
-          </Footer>
-        </Container>
-      </View>
+          </Left>
+          <Body>
+            <Title>Boomer</Title>
+          </Body>
+          <Button transparent onPress={() => navigate('Login')}>
+            <Icon name="menu" />
+          </Button>
+        </Header>
+        <Content>
+          <Text>Welcome on the home page</Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
+    // <Button
+    //   title="Go to Jane's profile"
+    //   onPress={() => navigate('Profile', { name: 'Jane' })}
+    // />
   }
 }
 
--- Boomer-RN-App/src/containers/login.js
+++ Boomer-RN-App/src/containers/login.js
@@ -1,7 +1,66 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
+import {
+  Container,
+  Header,
+  Title,
+  Card,
+  CardItem,
+  Item,
+  Label,
+  Input,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Form,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
 // import { connect } from 'react-redux';
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    textAlign: 'center' /* Pas appliqué ! WTF ? */
+  },
+  loginContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'red' /* Taille du container ? Full Size ! */
+  },
+  loginTitle: {
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: '5%',
+    color: '#FFFFFF'
+  },
+  loginInput: {
+    width: '90%',
+    marginRight: '10%'
+  },
+  loginForm: {
+    width: '90%',
+    height: 250,
+    marginTop: '10%',
+    marginLeft: '5%',
+    marginRight: '5%',
+    backgroundColor: '#16202c',
+    borderRadius: 10,
+    borderColor: '#ed9019'
+  },
+  loginButton: {
+    width: '90%',
+    marginTop: '10%',
+    marginLeft: '5%',
+  },
+  loginTextButton: {
+    textAlign: 'center' /* Pas appliqué ! WTF ? */
+  }
+});
 
 export class Login extends Component {
   static propTypes = {
@@ -10,9 +69,34 @@ export class Login extends Component {
 
   render() {
     return (
-      <View>
-        <Text> prop </Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title style={styles.headerTitle}>Boomer</Title>
+          </Body>
+        </Header>
+        <Content>
+          <View style={styles.loginContainer}>
+            <Form style={styles.loginForm}>
+              <Text style={styles.loginTitle}>
+                Login
+              </Text>
+              <Item floatingLabel style={styles.loginInput}>
+                <Label>Username</Label>
+                <Input />
+              </Item>
+              <Item floatingLabel style={styles.loginInput}>
+                <Label>Password</Label>
+                <Input />
+              </Item>
+              <Button rounded style={styles.loginButton}>
+                <Text style={styles.loginTextButton}>Login</Text>
+              </Button>
+            </Form>
+          </View>
+        </Content>
+
+      </Container>
     );
   }
 }
